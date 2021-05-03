26d183e5cc5da258699117a52d891bcc2f964bce
KelvenH <73842369+KelvenH@users.noreply.github.com>
2021-05-03 22:15:38 +0100

    added outline of initial JS scripts (js)

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+// Wait until DOM loaded before running the game
+
+// Event listeners for buttons
+
+// Device upgrades A - User Selection
+
+// Device upgrades B - Update Gameplay Stats
+
+// Device upgrades C - Update Balance (post transaction)
+
+// Run game (initiate game cycle stages)
+
+// Game stage A - generate coin ID
+
+// Game stage B - check if coin ID matches miner ID
+
+// Game Stage C - Calculate costs
+
+// Game Stage D - Calculate Outcome 
+
+// Update Balance (post game-cycle)
+
+// Reset screen for new game?
